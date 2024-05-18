522cc9f4b9272cc6cfb2704d47235e15e8547593
Antonio Lua <antonio28luah@gmail.com>
2024-05-17 21:04:03 -0600

    tarea6-unidad4

--- src/Biblioteca/libros/LibroTerror.java
+++ src/Biblioteca/libros/LibroTerror.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class LibroTerror  extends Libro{
+public class LibroTerror extends Libro {
     private SubGeneroTerror subGeneroTerror;
 
     public LibroTerror(String nombre, String autor, String editorial, LocalDate fechaDePubliacacion, Genero genero, double precio, int stock, SubGeneroTerror subGeneroTerror) {
@@ -24,15 +24,15 @@ public class LibroTerror  extends Libro{
         return subGeneroTerror;
     }
 
-    public static void registrarLibro(){
+    public static void registrarLibro() {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> datosComun = LibrosUtils.obtenerDatosComun();
-        String nombre= datosComun.get(0);
+        String nombre = datosComun.get(0);
         String autor = datosComun.get(1);
         String editorial = datosComun.get(2);
         LocalDate fechaDePubliacacion = LocalDate.parse(datosComun.get(3));
-        double precio=Double.parseDouble(datosComun.get(4));
-        int stock=Integer.parseInt(datosComun.get(5));
+        double precio = Double.parseDouble(datosComun.get(4));
+        int stock = Integer.parseInt(datosComun.get(5));
 
         System.out.println("Ingrese el subgenero del libro");
         System.out.println("Seleccione una opcion: ");
@@ -40,7 +40,7 @@ public class LibroTerror  extends Libro{
         System.out.println("2. Crimen");
 
         int opcionSubgenero = 0;
-        opcionSubgenero= sc.nextInt();
+        opcionSubgenero = sc.nextInt();
         SubGeneroTerror subgeneroInput = null;
         if (opcionSubgenero == 1) {
             subgeneroInput = SubGeneroTerror.PSICOLOGICO;
@@ -50,33 +50,30 @@ public class LibroTerror  extends Libro{
         }
 
 
-
-        LibroTerror libroTerror = new LibroTerror(nombre, autor, editorial, fechaDePubliacacion,Genero.TERROR, precio, stock, subgeneroInput);
-        System.out.println("Libro "+libroTerror.getNombre()+" registrado exitosamente");
+        LibroTerror libroTerror = new LibroTerror(nombre, autor, editorial, fechaDePubliacacion, Genero.TERROR, precio, stock, subgeneroInput);
+        System.out.println("Libro " + libroTerror.getNombre() + " registrado exitosamente");
     }
+
     @Override
-    public void filtrarPorPrecio(double precio){
-     Biblioteca.libros.get(Genero.TERROR).stream()
-             .filter(libro -> libro.getPrecio() >= precio)
-             .forEach(libro -> System.out.println(libro.toString()));
+    public void filtrarPorPrecio(double precio) {
+        Biblioteca.libros.get(Genero.TERROR).stream()
+                .filter(libro -> libro.getPrecio() >= precio)
+                .forEach(libro -> System.out.println(libro.toString()));
 
-     //conviertes, filtras, imprimes
+        //conviertes, filtras, imprimes
     }
 
-    public void filtrarPorRangoPrecio(double precio1,double precio2){
+    public void filtrarPorRangoPrecio(double precio1, double precio2) {
         Biblioteca.libros.get(Genero.TERROR).stream()
-                .filter(libro -> libro.getPrecio() >= precio1 && libro.getPrecio()<=precio2)
+                .filter(libro -> libro.getPrecio() >= precio1 && libro.getPrecio() <= precio2)
                 .forEach(libro -> System.out.println(libro.toString()));
     }
 
-    public void filtrarPorLetra(char letra){
+    public void filtrarPorLetra(char letra) {
         Biblioteca.libros.get(Genero.TERROR).stream()
                 .filter(libro -> libro.getNombre().charAt(0) == letra)
                 .forEach(libro -> System.out.println(libro.toString()));
     }
-
-
-
 
 
 }
